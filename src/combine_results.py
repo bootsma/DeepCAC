6608af6c6dd00ade10734f5c0c710d904fde408a
bootsma <bootsma@gmail.com>
2022-03-31 13:17:44 -0400

    post processing utilities

--- src/combine_results.py
+++ src/combine_results.py
@@ -4,12 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import PercentFormatter
 
+#from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import plot_confusion_matrix
+
 def run_parser():
     parser = argparse.ArgumentParser(description="combine_results.py\n Takes CAC scoring csv results output by DeepCAC "
                                                  "(deep_cac_csv) and combines them with CAC scores from another source (source_cac)")
     parser.add_argument('deep_cac_csv', type=str, help="csv filename output from DeepCAC")
     parser.add_argument('source_cac_csv', type=str, help="csv filename of additional CAC scores")
+    parser.add_argument('-s','--source_add',action='store_true')
     parser.add_argument('-p','--plot',action='store_true')
+    parser.add_argument('-p2','--plot_2',action='store_true', help="plot data seperated into contrast/non-contrast" )
     return parser.parse_args()
 
 def read_source_data(csv_filename):
@@ -72,6 +77,21 @@ def read_deep_cac(csv_filename ):
 
 
 def combine_csv_data(output_filename, deepcac_data, source_header, source_data, max_images_per_patient, source_add=True):
+    """
+
+    Args:
+        output_filename:
+        deepcac_data:
+        source_header:
+        source_data:
+        max_images_per_patient:
+        source_add:  this adds the data in the format of the original csv spread sheet if True, this means if the
+         data is missing for a patient it is still in the spread sheet and multiple images are appended as columns to the row.
+         If false only patients with data are stored and new rows are created for each image.
+
+    Returns:
+
+    """
 
     with open(output_filename, 'wb') as csv_file:
         writer = csv.writer(csv_file)
@@ -90,72 +110,157 @@ def combine_csv_data(output_filename, deepcac_data, source_header, source_data, 
             id = data[source_id_index]
             pred_vals = deepcac_data.get(id)
             exist = False
-            if pred_vals is None:
-                print('No DeepCAC predictions for ID:{}'.format(id))
-
-                data.append('')
-                data.append('')
-            else:
-                exist = True
-                for val in pred_vals:
-                    data.append(val)
-
             if source_add:
+                if pred_vals is None:
+                    print('No DeepCAC predictions for ID:{}'.format(id))
+                    data.append('')
+                    data.append('')
+                else:
+
+                   for val in pred_vals:
+                        data.append(val)
                 writer.writerow(data)
                 new_data.append(data)
-            elif not source_add and exist:
+            elif pred_vals is not None:
+                orig_data = list(data)
+                i=0
+                for val in pred_vals:
+                    i = i + 1
+                    if i>3:
+                        writer.writerow(data)
+                        new_data.append(data)
+                        data = orig_data
+                        i=0
+                    data.append(val)
                 writer.writerow(data)
                 new_data.append(data)
+
+
         return new_data, new_header
 
 
+def plot_data_set( truth_class, predicted_class, truth_cac_score, pred_cac_score, set_name = ""):
 
+    diff_cac = [float(pred)-float(truth) for pred,truth in zip(predicted_class, truth_class)]
+    bin_list = [-3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5]
+    fig = plt.figure()
+    plt.hist(diff_cac, bins=bin_list, density=True)
+    plt.ylim([0, 1])
+    plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+    plt.ylabel('Percent of Patients (N Patients = {})'.format(len(truth_class)))
+    plt.xlabel('[Chest CT CAC Visual Score] -[Deep CAC Class]')
+    plt.title('CAC Class Error Distribution ({})'.format(set_name))
+    #plt.show()
+    fig = plt.figure()
+    plt.plot(truth_cac_score, pred_cac_score, 'o')
+
+
+    m, b = np.polyfit(truth_cac_score, pred_cac_score, 1)
+    plt.plot(truth_cac_score, m * np.array(truth_cac_score) + b)
+    maxval = max([max(truth_cac_score), max(pred_cac_score)])
+    plt.xlim([0, maxval])
+    plt.ylim([0, maxval])
+    plt.xlabel('Cardiac CT CAC Score')
+    plt.ylabel('DeepCAC Predicted CAC Score')
+    plt.title('Correlation ({})'.format(set_name))
+    corr = np.corrcoef(truth_cac_score, pred_cac_score)[0, 1]
+    plt.text(500, 3000, 'Fit: y={:.2f}x+{:.2f}\nCorrelation(x,y)={:.2f}'.format(m, b, corr))
+
+    fig, axs = plt.subplots(4)
+
+    #fig.suptitle('{} Data'.format(set_name))
+
+    tmp = [(float(x)) for x in truth_class]
+    t_class = np.array(tmp)
+    tmp = [(float(x)) for x in pred_class]
+    p_class = np.array(tmp)
+    class_range_str = ['[0]','[1-99]','[100-299]','[>300]']
+    for cac_class in [0,1,2,3]:
+        index = np.where(t_class == cac_class)
+        t = t_class[index[0]]
+        p = p_class[index[0]]
+        error = np.sum(t == p)
+        accuracy = float(error)/len(t)
+        tmp_string ='{} Class {} {} Accuracy: {}, N: {}'.format(set_name,cac_class, class_range_str[cac_class], accuracy, len(t))
+        print(tmp_string)
+        diff_cac =  (p-t)
+        axs[cac_class].hist(diff_cac, bins=bin_list, density=True)
+        axs[cac_class].set_title(tmp_string, )
+        axs[cac_class].set_ylim([0,1])
+        axs[cac_class].yaxis.set_major_formatter(PercentFormatter(1))
+
+
+    
+
+    print('Correlation: {}'.format(corr))
 
 if __name__ == "__main__":
 
     args = run_parser()
     data, max_images_per_patient = read_deep_cac(args.deep_cac_csv)
-    print(data)
-    print('MAX: {}'.format(max_images_per_patient))
+
+    source_add = args.source_add
+    if max_images_per_patient > 1 and source_add:
+        print("WARNING: There are up to {} images for some patients, if plotting only first image in data is included.".format(max_images_per_patient))
+        print("TURN OFF SOURCE ADD to create new entry for each image.")
+
     source_header, source_data = read_source_data(args.source_cac_csv)
 
-    output_filename = args.source_cac_csv[:args.source_cac_csv.rfind('.')] + '_deepcac.csv'
-    combined_data, combined_header = combine_csv_data(output_filename, data, source_header, source_data, max_images_per_patient,False)
+
+    if source_add:
+        output_filename = args.source_cac_csv[:args.source_cac_csv.rfind('.')] + '_sa_deepcac.csv'
+    else:
+        output_filename = args.source_cac_csv[:args.source_cac_csv.rfind('.')] + '_deepcac.csv'
+
+    combined_data, combined_header = combine_csv_data(output_filename, data, source_header, source_data, max_images_per_patient, source_add)
     if args.plot:
-        truth_cac_index = combined_header.index('Chest CT CAC Visual score (0 absent, 1 mild, 2 moderate, 3 severe)')
-        deep_cac_index = combined_header.index('DeepCAC Class_pred')
 
-        diff_cac =[float(x[deep_cac_index])- float(x[truth_cac_index]) for x in combined_data]
-        bin_list = [-3.5,-2.5,-1.5,-0.5,0.5,1.5,2.5,3.5]
-        plt.hist(diff_cac,bins = bin_list,density =True)
-
-        plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
-        plt.ylabel('Percent of Patients (N Patients = {})'.format(len(combined_data)))
-        plt.xlabel('[Chest CT CAC Visual Score] -[Deep CAC Class]')
-        plt.title('CAC Class Error Distribution')
-        plt.show()
+        truth_class_index = combined_header.index('Chest CT CAC Visual score (0 absent, 1 mild, 2 moderate, 3 severe)')
+        deep_class_index = combined_header.index('DeepCAC Class_pred')
+        truth_class = [ x[truth_class_index] for x in combined_data]
+        pred_class = [x[deep_class_index] for x in combined_data]
 
         truth_cac_index = combined_header.index('Cardiact CT CAC score')
         deep_cac_index = combined_header.index('DeepCAC CAC_pred')
         truth_cac_score = [float(x[truth_cac_index]) for x in combined_data]
         pred_cac_score = [float(x[deep_cac_index]) for x in combined_data]
 
-        plt.plot(truth_cac_score, pred_cac_score,'o')
+        plot_data_set(truth_class, pred_class, truth_cac_score, pred_cac_score,'ALL DATA')
 
-        m,b=np.polyfit(truth_cac_score,pred_cac_score,1)
-        plt.plot(truth_cac_score, m*np.array(truth_cac_score)+b)
-        maxval = max([max(truth_cac_score ), max(pred_cac_score)])
-        plt.xlim([0, maxval])
-        plt.ylim([0, maxval])
-        plt.xlabel('Cardiac CT CAC Score')
-        plt.ylabel('DeepCAC Predicted CAC Score')
-        corr = np.corrcoef(truth_cac_score, pred_cac_score)[0,1]
-        plt.text(500,3000, 'Fit: y={:.2f}x+{:.2f}\nCorrelation(x,y)={:.2f}'.format(m,b,corr))
-        plt.show()
-        print('Correlation: {}'.format(corr))
+    if args.plot_2:
+
+        truth_class_index = combined_header.index('Chest CT CAC Visual score (0 absent, 1 mild, 2 moderate, 3 severe)')
+        deep_class_index = combined_header.index('DeepCAC Class_pred')
+        contrast_index = combined_header.index('Chest CT Contrast-enhanced (No=0, Yes=1) ')
+
+        truth_class = [ x[truth_class_index] for x in combined_data if float(x[contrast_index]) > 0]
+        pred_class = [x[deep_class_index] for x in combined_data if float(x[contrast_index]) > 0]
+
+        truth_cac_index = combined_header.index('Cardiact CT CAC score')
+        deep_cac_index = combined_header.index('DeepCAC CAC_pred')
+        truth_cac_score = [float(x[truth_cac_index]) for x in combined_data if float(x[contrast_index]) > 0]
+        pred_cac_score = [float(x[deep_cac_index]) for x in combined_data if float(x[contrast_index]) > 0]
+
+        plot_data_set(truth_class, pred_class, truth_cac_score, pred_cac_score,'CONTRAST')
+
+        truth_class_index = combined_header.index('Chest CT CAC Visual score (0 absent, 1 mild, 2 moderate, 3 severe)')
+        deep_class_index = combined_header.index('DeepCAC Class_pred')
+        contrast_index = combined_header.index('Chest CT Contrast-enhanced (No=0, Yes=1) ')
+
+        truth_class = [ x[truth_class_index] for x in combined_data if float(x[contrast_index]) < 1]
+        pred_class = [x[deep_class_index] for x in combined_data if float(x[contrast_index]) < 1]
+
+        truth_cac_index = combined_header.index('Cardiact CT CAC score')
+        deep_cac_index = combined_header.index('DeepCAC CAC_pred')
+        truth_cac_score = [float(x[truth_cac_index]) for x in combined_data if float(x[contrast_index]) < 1]
+        pred_cac_score = [float(x[deep_cac_index]) for x in combined_data if float(x[contrast_index]) < 1]
 
 
+        plot_data_set(truth_class, pred_class, truth_cac_score, pred_cac_score,'NO CONTRAST')
+    plt.show()
 
+
+    test = 2
 
 
 
---
+++ src/visualize_images.py
@@ -0,0 +1,179 @@
+import argparse
+import csv
+import shutil
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+def run_parser():
+    parser = argparse.ArgumentParser(description="visualize_images.py\n Takes CAC scoring csv results output by "
+                                                 "combine_results "
+                                                 "and looks for the segmented CAC data and original volume to create "
+                                                 "a visualization\n "
+                                                 "of the results. The figures are output in 3rd step directory in"
+                                                 "cropped_qc.")
+
+    parser.add_argument('result_dir', type=str, help="Main output dir from DeepCAC")
+    parser.add_argument('cac_file', type=str, help="CSV File from combine_results.py")
+    return parser.parse_args()
+
+def read_source_data(csv_filename):
+
+    with open(csv_filename) as csv_file:
+        reader = csv.reader(csv_file)
+        header = next(reader)
+        data = []
+        for row in reader:
+            data.append(row)
+
+        return header, data
+
+def plot_prediction(base_dir, id, img, cac_score=-1, deepcac_score=-1):
+
+
+
+    base_filename = id + '.' + img
+    file_pred = base_filename + '_pred.npy'
+    file_img = base_filename + '_img.npy'
+    file_img3071 = base_filename + '_img_3071.npy'
+
+
+    model_dir = os.path.join(base_dir, 'step3_cacseg', 'model_output','npy')
+    cropped_dir = os.path.join(base_dir, 'step3_cacseg', 'cropped')
+
+
+    outdir = os.path.join(base_dir,'step4_cac_score', 'visuals')
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+
+    try:
+
+        filepath = os.path.join(model_dir, file_pred)
+        prd = np.load(filepath)
+
+        filepath = os.path.join(cropped_dir, file_img)
+        img = np.load(filepath)
+
+        filepath = os.path.join(cropped_dir, file_img3071)
+        img3071 = np.load(filepath)
+
+    except Exception as e:
+        print('Could not load files, Exception: {}'.format(e))
+        return outdir
+
+
+    #print('p: ', prd.shape)
+    #print('i: ', img.shape)
+    #print('i3071: ', img3071.shape)
+
+    mask_thresh = 0.1
+    prd_mask = np.copy(prd)
+    prd_mask[prd_mask < mask_thresh] = 0
+    prd_mask[prd_mask > 0] = 1
+
+
+
+
+    max_axis0 = np.sum(np.sum(prd_mask, axis=2), axis=1)
+    index_axis0 = np.argmax(max_axis0)
+    max_axis1 = np.sum(np.sum(prd_mask, axis=2), axis=0)
+    index_axis1 = np.argmax(max_axis1)
+    max_axis2 = np.sum(np.sum(prd_mask, axis=0), axis=0)
+    index_axis2 = np.argmax(max_axis2)
+
+    fig, ax = plt.subplots(3, 3, figsize=(32, 16))
+    sz = img.shape
+
+    ax[0, 0].imshow(img3071[index_axis0, :, :], cmap='gray')
+    ax[0, 0].set_title('Slice index {}'.format(index_axis0))
+    ax[0, 1].imshow(img3071[:, index_axis1, :], cmap='gray')
+    ax[0, 1].set_title('Slice index {}'.format(index_axis1))
+    ax[0, 2].imshow(img3071[:, :, index_axis2], cmap='gray')
+    ax[0, 2].set_title('Slice index {}'.format(index_axis2))
+
+    ax[1, 0].imshow(img3071[index_axis0, :, :], cmap='gray')
+    ax[1, 1].imshow(img3071[:, index_axis1, :], cmap='gray')
+    ax[1, 2].imshow(img3071[:, :, index_axis2], cmap='gray')
+    ax[1, 0].imshow(prd_mask[index_axis0, :, :], cmap='jet', alpha=0.5)
+    ax[1, 1].imshow(prd_mask[:, index_axis1, :], cmap='jet', alpha=0.5)
+    ax[1, 2].imshow(prd_mask[:, :, index_axis2], cmap='jet', alpha=0.5)
+
+    ax[2, 0].imshow(prd[index_axis0, :, :], cmap='gray')
+    ax[2, 1].imshow(prd[:, index_axis1, :], cmap='gray')
+    ax[2, 2].imshow(prd[:, :, index_axis2], cmap='gray')
+    """
+    ax[2, 0].imshow(prd[index_axis0, :, :], cmap='jet', alpha=0.5)
+    ax[2, 1].imshow(prd[:, index_axis1, :], cmap='jet', alpha=0.5)
+    ax[2, 2].imshow(prd[:, :, index_axis2], cmap='jet', alpha=0.5)
+    """
+
+    diff_score = float(deepcac_score) - float(cac_score)
+    plt.suptitle('CAC:{}, Deep-CAC:{}, Diff:{}'.format(cac_score,deepcac_score, diff_score))
+    fig.tight_layout()
+
+    filename = os.path.join(outdir, base_filename)
+
+    filename += '_step4_{}_pred.png'.format(abs(diff_score))
+
+    plt.savefig(filename)
+    plt.show()
+    plt.close(fig)
+
+
+    model_dir_s1 = os.path.join(base_dir, 'step1_heartloc', 'model_output', 'png')
+    model_dir_s2 = os.path.join(base_dir, 'step2_heartseg', 'model_output', 'png')
+    model_dir_s3 =  os.path.join(base_dir, 'step3_cacseg', 'cropped_qc')
+    file_img = base_filename + '_.png'
+    try:
+        file_cpy = base_filename + '_step1_loc.png'
+        shutil.copyfile(os.path.join(model_dir_s1,file_img), os.path.join(outdir, file_cpy))
+    except Exception as e:
+        print('No image file for step1: {}'.format(e))
+
+    try:
+        file_cpy = base_filename + '_step2_seg.png'
+        shutil.copyfile(os.path.join(model_dir_s2,file_img), os.path.join(outdir, file_cpy))
+    except Exception as e:
+        print('No image file for step2: {}'.format(e))
+
+    try:
+        file_img = base_filename + '.png'
+        file_cpy = base_filename + '_step3_cacseg.png'
+        shutil.copyfile(os.path.join(model_dir_s3,file_img), os.path.join(outdir, file_cpy))
+    except Exception as e:
+        print('No image file for step1: {}'.format(e))
+
+    base_filename + '_.png'
+
+
+
+if __name__ == "__main__":
+    args = run_parser()
+    header, data = read_source_data(args.cac_file)
+
+    base_dir = args.result_dir
+
+
+    cropped_qc_dir = os.path.join(base_dir, 'step3_cacseg', 'cropped_qc')
+
+    print('Looking for data in: {}'.format(base_dir))
+
+    # read data1
+    index_id = header.index('RESEARCH_ID')
+
+    #todo There can be more than one ImageId for a patient
+    index_img = header.index('ImageId')
+    index_cac_score = header.index('Cardiact CT CAC score')
+    index_deepcac_score = header.index('DeepCAC CAC_pred')
+
+    for row in data:
+        id = row[index_id] #'AB24600UN.R'
+        img = row[index_img] #'CT_426cd8da-11237ca4-b7d2e943-4d51b36b-f1a352b5'
+        cac_score =row[index_cac_score]
+        deepcac_score= row[index_deepcac_score]
+        outdir=plot_prediction(base_dir, id, img, cac_score, deepcac_score)
+
+    print('Output data in: \n{}'.format(outdir))
